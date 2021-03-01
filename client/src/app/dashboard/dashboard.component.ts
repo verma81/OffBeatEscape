04ba9b7470c657e1e19ec120819c7c85b641ba49
Sidhartha Verma <sidharthaverma@Sidharthas-MacBook-Air.local>
2021-03-01 15:44:31 +0530

    s

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DashBoardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +9,16 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private dashboardService: DashBoardService) { }
 
   ngOnInit(): void {
     localStorage.removeItem('logged_in_noramlly');
   }
 
   logout(): void {
+    this.dashboardService.logoutUser().subscribe( data => {
+      console.log(data);
+    });
     this.router.navigate(['/login']);
   }
 
