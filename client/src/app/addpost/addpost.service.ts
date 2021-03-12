584ca56c3a6f961523f8e660a03718d97f22f646
sanjuatwal <sanjuatwal007@gmail.com>
2021-03-12 11:00:40 +0530

    post conf

---
+++ client/src/app/addpost/addpost.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Post} from "./addpost.model"
+
+class ImageSnippet{
+  constructor(public src: string, public file: File) {}
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AddPostService {
+  // @ts-ignore
+  selectedFile : ImageSnippet
+
+    constructor(private http: HttpClient){
+
+	}
+
+  // addPost(post: Post){
+  //   const formData = new FormData()
+  //   console.log(post.description)
+  //   formData.append('description', post.description)
+  //   formData.append('title', post.title)
+  //   return this.http.post('http://localhost:3000/post/addPost',{
+  //     title : post.title,
+  //     description : post.description,
+  //   })
+  // }
+
+    addPost(post: Post){
+    const formData = new FormData()
+    console.log(this.selectedFile)
+    formData.append('description', post.description)
+    formData.append('title', post.title)
+    return this.http.post('http://localhost:3000/post/addPost', formData)
+  }
+
+}
---
+++ client/src/app/commonservices/postid.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostidService {
+  postId: string = ''
+
+  constructor() { }
+}
