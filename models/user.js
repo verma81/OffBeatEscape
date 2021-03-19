fe1a1090eefa974636e187645d2a3441c7536acd
Kanav Malik <kanav@renebyte.xyz>
2021-03-19 13:26:14 +0530

    add friends

--- models/user.js
+++ models/user.js
@@ -4,13 +4,53 @@ const user = new mongoose.Schema({
   password: String,
   googleId: {
     required: false,
-    type: String
+    type: String,
   },
   facebookId: {
     required: false,
-    type: String
-  }
-
+    type: String,
+  },
+ 
+  friends: {
+    type: [
+      {
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+          index: true,
+          required: true,
+          auto: true,
+        },
+        username: { type: String },
+        profileImage: { type: String },
+        status: { type: String },
+        sentByMe: { type: Boolean },
+      },
+    ],
+  },
+  notifications: {
+    type: [
+      {
+        _id: { type: String },
+        username: { type: String },
+        profileImage: { type: String },
+        status: { type: String },
+        sentByMe: { type: Boolean },
+        _id: { type: String },
+        type: { type: String },
+        content: { type: String },
+        profileImage: { type: String },
+        createdAt: { type: Date },
+      },
+    ],
+  },
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("User", user);
