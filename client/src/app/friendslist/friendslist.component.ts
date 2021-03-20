0d4e4bf539dd9ab81674133499b33f10d0b36a93
Sidhartha Verma <sidharthaverma@Sidharthas-MacBook-Air.local>
2021-03-20 12:52:11 +0530

    Integration Changes for add friends

--- client/src/app/friendslist/friendslist.component.ts
+++ client/src/app/friendslist/friendslist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashBoardService } from '../dashboard/dashboard.service';
 
 @Component({
   selector: 'app-friendslist',
@@ -8,36 +9,64 @@ import { Component, OnInit } from '@angular/core';
 export class FriendslistComponent implements OnInit {
 
   friendName: string = "";
+
   friendsList: Array<any> = [];
-  constructor() { }
+
+  constructor(
+    private dashboardService: DashBoardService
+  ) { }
 
   ngOnInit(): void {
-    this.friendsList.push(
-      {
-        friendName: "Sid",
-        friendRole: "friend"
-      },
-      {
-        friendName: "Sid1",
-        friendRole: "friend"
-      },
-      {
-        friendName: "Sid1",
-        friendRole: "friend"
-      },
-      {
-        friendName: "Sid1",
-        friendRole: "friend"
-      },
-      {
-        friendName: "Sid1",
-        friendRole: "friend"
-      }
-    )
+    this.dashboardService.getUsersList().subscribe(data => {
+      this.friendsList = data;
+      this.filterFriendsList(data);
+    });
   }
 
-  calculateValue(): void {
-    console.log(this.friendName);
+  filterFriendsList(usersList: any): void {
+    const alreadySentFriendRequestList: any = [];
+    const currentUser = JSON.parse(this.getLoggedInUser());
+    
+    if(currentUser.friends && currentUser.friends.length > 0) {
+      currentUser.friends.map((friendRequestSent: any) => {
+        alreadySentFriendRequestList.push(friendRequestSent);
+      });
+    }
+    
+    if(alreadySentFriendRequestList && alreadySentFriendRequestList.length > 0) {
+      for(var i = 0; i < usersList.length; i++) {
+        for(var j = 0 ; j < alreadySentFriendRequestList.length; j++) {
+          if(usersList[i]._id == alreadySentFriendRequestList[j]._id) {
+            console.log("foud", usersList[i]._id, i);
+            usersList[i]['friendRequestSent'] = true;
+          }
+        }
+      }
+    }
+    
+    const tempUserList = usersList.filter((user: any) => {
+      return (!user.friendRequestSent === true)
+    });
+
+
+    if(tempUserList && tempUserList.length > 0) {
+      this.friendsList = tempUserList;
+    }
+    
+  }
+
+  sendFriendRequestToUser(friend: any): void {
+    const currentUser = JSON.parse(this.getLoggedInUser());
+    const friendRequestPayLoad = {
+      '_id': friend._id
+    }
+    this.dashboardService.sendFriendRequest(currentUser, friendRequestPayLoad).subscribe(data => {
+
+    });
+  }
+
+  getLoggedInUser(): any {
+    return JSON.parse(JSON.stringify(localStorage.getItem('currentUser')));
   }
 
 }
